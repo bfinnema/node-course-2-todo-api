393ed36c39fa19d6048ab7625b54e6864d1bbb10
bfinnema <bfinnema@cisco.com>
2017-05-29 17:11:44 +0200

    Add POST /users/login route

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,3 +1,5 @@
+// const bcrypt = require('bcryptjs');
+
 var {User} = require('./../models/user.js');
 
 var authenticate = (req, res, next) => {
@@ -15,5 +17,32 @@ var authenticate = (req, res, next) => {
         res.status(401).send();
     });
 };
+
+/*
+var login = (req, res, next) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    console.log(`Email: ${email}, Password: ${password}`);
+
+    User.findByEmail(email).then((user) => {
+        if (!user) {
+            console.log(`User was not found`);
+            return Promise.reject();
+        };
+        console.log(`Found user: ${user}`);
+        bcrypt.compare(password, user.password, (err, res) => {
+            console.log(`Result of the compare: ${res}`);
+            if (res) {
+                req.user = user;
+                next();
+            } else {
+                
+            };
+        });
+    }).catch((e) => {
+        res.status(401).send();
+    });
+};
+*/
 
 module.exports = {authenticate};
